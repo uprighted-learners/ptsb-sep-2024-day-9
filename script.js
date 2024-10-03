09f6f1f9de75a8ef9293afd0ee41e3be182c2f8e
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-10-03 17:42:49 -0500

    some conditionals

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("hello world"); 
+// examples of booleans
+let isLoggedIn = false;
+let isAdmin = true;
+
+if (isLoggedIn) {
+    console.log("User is logged in");
+    if (isAdmin) {
+        console.log("User is an admin");
+    } else {
+        console.log("User is not an admin");
+    }
+} else {
+    console.log("User is not logged in");
+}
+
+// evaluating conditions with numbers
+let a = 10;
+let b = 5;
+let isGreaterThan = a > b;
+console.log(isGreaterThan);
+
+if (isGreaterThan) {
+    console.log("a is greater than b");
+}
